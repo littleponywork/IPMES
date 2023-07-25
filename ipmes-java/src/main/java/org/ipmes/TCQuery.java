03fa0da96ac04b26f76f9a730b513d97bd9f4846
Datou0718 <lyck92@gmail.com>
2023-07-25 16:41:41 +0800

    add comment

--- ipmes-java/src/main/java/org/ipmes/TCQuery.java
+++ ipmes-java/src/main/java/org/ipmes/TCQuery.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class TCQuery {
 
+    // we can later sort quiries by size to greedy choose
     int size;
     ArrayList<Integer> query;
 
@@ -12,20 +13,34 @@ public class TCQuery {
         this.query = query;
     }
 
+    // DFS to find out possible TC subqueries
+    // constraints:
+    // 1. new edge is not in the TC subquery yet
+    // 2. new edge need to have share node with TC subquery
+    // 3. follow temporal rules
+    // so we handle edges in temporal order, then check 1. and 2.
     public static ArrayList<TCQuery> generate_TCQueries(
             DependencyGraph TemporalRelation,
             PatternGraph SpatialRelation,
             ArrayList<Integer> current_path,
             int current_ID) {
+        // the TC subquery set to be return
         ArrayList<TCQuery> ret = new ArrayList<TCQuery>();
+        // extract children
         ArrayList<Integer> children = TemporalRelation.getChildren(current_ID);
+
         int numChildren = children.size(), current_path_size = current_path.size();
+
+        // add the present path to the subquery set
         ArrayList<Integer> ret_path = new ArrayList<Integer>(current_path);
         ret.add(new TCQuery(current_path_size, ret_path));
+
+        // deal with every child
         for (int i = 0; i < numChildren; i++) {
             int childId = children.get(i);
             for (int j = 0; j < current_path_size; j++) {
                 int current_path_edgeId = current_path.get(j);
+                // checking 1. and 2.
                 if (!(SpatialRelation.getSharedNodes(childId, current_path_edgeId).isEmpty()
                         || current_path.contains(childId))) {
                     ArrayList<Integer> new_current_path = current_path;
