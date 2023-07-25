2c0d89318ccf3d300219b5e60ceea0e1c4156d47
Datou0718 <lyck92@gmail.com>
2023-07-25 11:24:57 +0800

    init

---
+++ ipmes-java/src/main/java/org/ipmes/SpatialRelation.java
@@ -0,0 +1,16 @@
+package main.java.org.ipmes;
+
+import java.util.*;
+
+public class SpatialRelation {
+    // FROM[id][edge[id].from]
+    ArrayList<ArrayList<Integer>> FROM = new ArrayList<ArrayList<Integer>>();
+    // TO[id][edge[id].to]
+    ArrayList<ArrayList<Integer>> TO = new ArrayList<ArrayList<Integer>>();
+
+    // initialize
+    public SpatialRelation() {
+
+    }
+
+}
