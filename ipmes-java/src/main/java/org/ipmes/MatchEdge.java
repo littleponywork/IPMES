0ddd30a3b8613b86c5b6c63c3298d9b8b716ec77
Daniel Liu <xyfc128@gmail.com>
2023-07-26 14:25:59 +0800

    add interface class to store match results

---
+++ ipmes-java/src/main/java/org/ipmes/MatchEdge.java
@@ -0,0 +1,23 @@
+package org.ipmes;
+
+public class MatchEdge {
+    public Integer getEventId() {
+        return 0;
+    }
+
+    public Integer getTimestamp() {
+        return 0;
+    }
+
+    public Integer getMatchPatternEdgeId() {
+        return 0;
+    }
+
+    public Integer getStartId() {
+        return 0;
+    }
+
+    public Integer getEndId() {
+        return 0;
+    }
+}
---
+++ ipmes-java/src/main/java/org/ipmes/MatchedResult.java
@@ -0,0 +1,13 @@
+package org.ipmes;
+
+import java.util.ArrayList;
+
+public class MatchedResult {
+
+    public Integer getMatchedTCQueryId() {
+        return 0;
+    }
+    public ArrayList<MatchEdge> getMatches() {
+        return new ArrayList<>();
+    }
+}
