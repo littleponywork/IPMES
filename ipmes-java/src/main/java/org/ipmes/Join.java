c97647947be3fdb1e70936a39c53a2e68757fbd8
Datou0718 <lyck92@gmail.com>
2023-07-27 15:28:25 +0800

    push join

--- ipmes-java/src/main/java/org/ipmes/Join.java
+++ ipmes-java/src/main/java/org/ipmes/Join.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class Join {
 
+    private boolean checkRelation(DataEdge candidate, DataEdge AlreadyIn) {
+        candidate.matched.
+        return true;
+    }
+
+    private boolean checkTime(DataEdge candidate, DataEdge AlreadyIn) {
+
+        return true;
+    }
+
     /*
      * ///////////////////////////////////////////////
      * join the result of TC subquery matching
@@ -11,22 +21,23 @@ public class Join {
 
     // output of Siddhi App would be ArrayList<MatchEdge>
     //
-    // MatchEdge{
-    // eventId: edge id in the input data
+    // DataEdge{
+    // dataId: edge id in the input data
     // timestamp: timestamp in the input data
     // startId: node id in the input data
     // endId: node id in the input data
-    // patternId: 0 based id we given to the edge
+    // matched: 0 based id we given to the edge
     // }
-
+    public void joinMatchResult(){
     ArrayList<ArrayList<DataEdge>> MatchResult = new ArrayList<ArrayList<DataEdge>>();
-    ArrayList<ArrayList<DataEdge>> ExpansionTable = new ArrayList<ArrayList<DataEdge>>();for(
-    ArrayList<DataEdge> subTCQ:MatchResult){
+    ArrayList<ArrayList<DataEdge>> ExpansionTable = new ArrayList<ArrayList<DataEdge>>();
+    boolean fit = true;
+    for(ArrayList<DataEdge> subTCQ : MatchResult){
             for(ArrayList<DataEdge> entry : ExpansionTable){
                 fit = true;
                 for(DataEdge candidate : subTCQ){
                     for(DataEdge AlreadyIn : entry){
-                        if(!checkRelation(candidate, AlreadyIn) || !checkTime(cnadidate, AlreadyIn)){
+                        if(!checkRelation(candidate, AlreadyIn) || !checkTime(candidate, AlreadyIn)){
                             fit = false;
                             break;
                         }
@@ -38,5 +49,5 @@ public class Join {
                     // put subTCQ in this entry;
             }
         }
-
-}}}
+    }
+}
