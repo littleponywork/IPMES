ef25d0304dd14c004bb3db3b4eedf3a40ec6dbcd
Daniel Liu <xyfc128@gmail.com>
2023-08-11 16:11:01 +0800

    remove debug line

--- ipmes-java/run_darpa.py
+++ ipmes-java/run_darpa.py
@@ -36,8 +36,8 @@ for pattern_name, file_prefix in pattern_file:
     print(f'Running pattern {pattern_name}')
     pdata: list[tuple[Popen, TextIOWrapper]] = []
     for graph in darpa_graphs:
-        # args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w 1000 --darpa ../data/darpa_patterns/{file_prefix} ../data/preprocessed/{graph}.csv" &> {out_file}']
-        args = ['bash', '-c', 'time -p -- mvn -q exec:java -Dexec.args="../data/patterns/TTP11 ../data/preprocessed/interval.csv"']
+        args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w 1000 --darpa ../data/darpa_patterns/{file_prefix} ../data/preprocessed/{graph}.csv"']
+        # args = ['bash', '-c', 'time -p -- mvn -q exec:java -Dexec.args="../data/patterns/TTP11 ../data/preprocessed/interval.csv"']
         print(args)
         out_file = open(f'../results/{pattern_name}_{graph}.out', 'w')
         pdata.append((Popen(args, stdout=out_file, stderr=out_file), out_file))
