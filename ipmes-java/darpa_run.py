0562d354f357e0d2891703fb463014d06f09e526
Daniel Liu <xyfc128@gmail.com>
2023-08-15 16:22:25 +0800

    seperate stdout and stderr

--- ipmes-java/darpa_run.py
+++ ipmes-java/darpa_run.py
@@ -15,15 +15,17 @@ os.environ['MAVEN_OPTS'] = '-Xmx100G'
 
 for pattern_name, file_prefix in pattern_file:
     print(f'Running pattern {pattern_name}')
-    pdata: list[tuple[Popen, TextIOWrapper]] = []
+    pdata: list[tuple] = []
     for graph in darpa_graphs:
         args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w 1000 --darpa ../data/darpa_patterns/{file_prefix} ../data/preprocessed/{graph}.csv"']
         # args = ['bash', '-c', 'time -p -- mvn -q exec:java -Dexec.args="../data/patterns/TTP11 ../data/preprocessed/interval.csv"']
         print(args)
         out_file = open(f'../results/{pattern_name}_{graph}.out', 'w')
-        pdata.append((Popen(args, stdout=out_file, stderr=out_file), out_file))
+        err_file = open(f'../results/{pattern_name}_{graph}.err', 'w')
+        pdata.append((Popen(args, stdout=out_file, stderr=err_file), out_file, err_file))
         
-    for proc, out_file in pdata:
+    for proc, out_file, err_file in pdata:
         proc.wait()
         out_file.close()
+        err_file.close()
 
