8adb9ea0f9ad3cd8488dc4e1255ef0af57ba0df1
Daniel Liu <xyfc128@gmail.com>
2023-08-15 15:50:29 +0800

    rename the darpa patterns

--- ipmes-java/darpa.py
+++ ipmes-java/darpa.py
@@ -1,9 +1,9 @@
 pattern_file = [
-    ('DDP1', 'TTP1-1_regex'),
-    ('DDP2', 'TTP1-2_regex'),
-    ('DDP3', 'TTP2_regex'),
-    ('DDP4', 'TTP3_regex'),
-    ('DDP5', 'TTP4_regex'),
+    ('DP1', 'TTP1-1_regex'),
+    ('DP2', 'TTP1-2_regex'),
+    ('DP3', 'TTP2_regex'),
+    ('DP4', 'TTP3_regex'),
+    ('DP5', 'TTP4_regex'),
 ]
 darpa_graphs = ['dd1', 'dd2', 'dd3', 'dd4']
 
