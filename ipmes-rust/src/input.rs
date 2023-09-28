99e8807aedd53f3c34d20dcf2e5e4a61d6f4fb71
Daniel Liu <xyfc128@gmail.com>
2023-09-28 16:22:11 +0800

    add required implementation for sorter

--- ipmes-rust/src/input.rs
+++ ipmes-rust/src/input.rs
@@ -1,12 +1,59 @@
-pub struct InputEdge;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+#[derive(Eq)]
+pub struct InputEdge {
+    pub timestamp: u64,
+    pub signature: String,
+    pub id: u64,
+    pub start: u64,
+    pub end: u64,
+}
 pub fn parse(string: &str) -> InputEdge {
     todo!()
 }
 
-pub struct Sorter;
+impl Ord for InputEdge {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.timestamp.cmp(&other.timestamp)
+    }
+}
 
+impl PartialOrd for InputEdge {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.timestamp.partial_cmp(&other.timestamp)
+    }
+}
+
+impl PartialEq<Self> for InputEdge {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+pub struct Sorter {
+    buffer: BinaryHeap<InputEdge>,
+}
 impl Sorter {
-    pub fn put(&mut self, edge: InputEdge) {
+    pub fn put(&mut self, edge: InputEdge) -> SortedIter {
+        let timestamp = edge.timestamp;
+        self.buffer.push(edge);
+        SortedIter {
+            buffer: &mut self.buffer,
+            timestamp,
+        }
+    }
+}
+
+pub struct SortedIter<'a> {
+    buffer: &'a mut BinaryHeap<InputEdge>,
+    timestamp: u64,
+}
+
+impl<'a> Iterator for SortedIter<'a> {
+    type Item = Vec<InputEdge>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         todo!()
     }
 }
