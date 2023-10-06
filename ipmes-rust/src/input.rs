fc9102e60574fc0524bb9b605cd1e05ac9afc88e
lbwei <0800680274united@gmail.com>
2023-10-06 10:39:01 +0800

    csv parsing good

--- ipmes-rust/src/input.rs
+++ ipmes-rust/src/input.rs
@@ -1,8 +1,10 @@
-use std::error::Error;
+use log::debug;
+use serde::Deserialize;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
+use std::error::Error;
 
-#[derive(Eq)]
+#[derive(Eq, Debug)]
 pub struct InputEdge {
     pub timestamp: u64,
     pub signature: String,
@@ -10,16 +12,45 @@ pub struct InputEdge {
     pub start: u64,
     pub end: u64,
 }
-// pub fn parse(string: &str) -> InputEdge {
-pub fn parse(string: &str) -> Result<(), Box<dyn Error>> {
+
+#[derive(Debug, serde::Deserialize)]
+struct Record {
+    pub timestamp1: f64,
+    pub timestamp2: f64,
+    pub signature: String,
+    pub id: u64,
+    pub start: u64,
+    pub end: u64,
+}
+
+pub fn parse(filename: &str) -> Result<(), Box<dyn Error>> {
+    println!("hi");
     let mut csv = csv::ReaderBuilder::new()
         .has_headers(false)
-        .from_path("./src/testcases/test.csv")?;
-    for data in csv.records() {
-        let record = data?;
-        println!("{:?}", record);
-    }
+        .from_path(filename)?;
+    for data in csv.deserialize() {
+        let record: Record = data?;
+        let timestamp1: u64 = (record.timestamp1 * 1000.0).round() as u64;
+        let timestamp2: u64 = (record.timestamp2 * 1000.0).round() as u64;
 
+        let edge1 = InputEdge {
+            timestamp: timestamp1,
+            signature: record.signature.clone(),
+            id: record.id,
+            start: record.start,
+            end: record.end,
+        };
+
+        let edge2 = InputEdge {
+            timestamp: timestamp2,
+            signature: record.signature,
+            id: record.id,
+            start: record.start,
+            end: record.end,
+        };
+
+        // println!("edge1: {:#?}\n edge2:{:#?}\n\n", edge1, edge2);
+    }
     Ok(())
 }
 
@@ -73,6 +104,7 @@ mod tests {
     use super::*;
     #[test]
     fn test() {
-        parse("a");
+        // println!("test");
+        let status = parse("src/testcases/test.csv");
     }
 }
