225db8e348a3ebd7d37a1a82648bffb8c010b474
lbwei <0800680274united@gmail.com>
2023-10-13 11:09:37 +0800

    add lifetime

--- ipmes-rust/src/sub_pattern_match.rs
+++ ipmes-rust/src/sub_pattern_match.rs
@@ -1,1 +1,7 @@
-pub struct SubPatternMatch;
+use crate::match_edge::MatchEdge;
+
+pub struct SubPatternMatch<'p> {
+    pub nodes: Vec<usize>,
+    // "MatchEdge" contains a pair: (edge_id_of_input_edge, pattern_edge)
+    pub edges: Vec<MatchEdge<'p>>,
+}
