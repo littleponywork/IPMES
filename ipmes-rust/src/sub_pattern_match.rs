a998a1c1e832a8ca9b771d7efd6863e5d0e25ec1
lbwei <0800680274united@gmail.com>
2023-10-19 22:44:20 +0800

    comment out join_layer

--- ipmes-rust/src/sub_pattern_match.rs
+++ ipmes-rust/src/sub_pattern_match.rs
@@ -7,7 +7,7 @@ pub struct SubPatternMatch<'p> {
     pub timestamp: u64,
     pub nodes: HashSet<usize>,
     // "MatchEdge" contains a pair: (edge_id_of_input_edge, pattern_edge)
-    // pub edges: Vec<MatchEdge<'p>>,
+    pub edges: Vec<MatchEdge<'p>>,
 
     // (pattern edge id, InputEdge)
     pub matched_edges_table: HashMap<usize, Rc<InputEdge>>,
