5cac5b95acb50944a2aa81a95925eedac04b9bd1
Daniel Liu <xyfc128@gmail.com>
2023-10-20 11:11:19 +0800

    more naive join

--- ipmes-rust/src/process_layers/naive_join_layer.rs
+++ ipmes-rust/src/process_layers/naive_join_layer.rs
@@ -1,20 +1,46 @@
+use std::collections::{BinaryHeap, HashSet};
+use std::rc::Rc;
+use crate::match_edge::MatchEdge;
 use crate::pattern_match::PatternMatch;
 use crate::sub_pattern_match::SubPatternMatch;
 
-struct NaiveJoinLayer<P>
+struct Entry<'p> {
+    earliest_time: u64,
+    match_edges: Vec<MatchEdge<'p>>,
+    num_nodes: usize,
+}
+
+impl<'p> From<Entry<'p>> for PatternMatch {
+    fn from(value: Entry<'p>) -> Self {
+        todo!()
+    }
+}
+
+pub struct NaiveJoinLayer<'p, P>
     where
         P: Iterator<Item = Vec<SubPatternMatch>>,
 {
     prev_layer: P,
+    table: HashSet<Rc<Entry<'p>>>,
+    order: BinaryHeap<Rc<Entry<'p>>>,
+    full_matches: Vec<PatternMatch>,
 }
 
-impl<P> Iterator for NaiveJoinLayer<P>
+impl<'p, P> NaiveJoinLayer<'p, P> {
+
+}
+
+impl<'p, P> Iterator for NaiveJoinLayer<'p, P>
     where
         P: Iterator<Item = Vec<SubPatternMatch>>,
 {
     type Item = PatternMatch;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        while self.full_matches.is_empty() {
+            let sub_pattern_matches = self.prev_layer.next()?;
+
+        }
+        self.full_matches.pop()
     }
 }
