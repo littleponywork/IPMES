7500163191622ff4fe17945f5296cd837788dee3
Daniel Liu <xyfc128@gmail.com>
2023-11-16 19:32:32 +0800

    update age limit experiment script

--- ipmes-java/age_limit_experiment.py
+++ ipmes-java/age_limit_experiment.py
@@ -2,7 +2,25 @@
 import subprocess
 from subprocess import Popen
 import os
+import json
+import argparse
 from results import parse_cputime
+
+parser = parser = argparse.ArgumentParser(
+                description='Age limit experiment')
+parser.add_argument('--start',
+                    default=100,
+                    help='start of the window size')
+parser.add_argument('--stop',
+                    default=3700,
+                    help='stop of the window size')
+parser.add_argument('--step',
+                    default=100,
+                    help='step of the window size')
+parser.add_argument('-r', '--re-run',
+                    default=3,
+                    help='re-run times')
+args = parser.parse_args()
 
 
 subprocess.run(['mvn', 'compile'], check=True)
@@ -12,12 +30,20 @@ if not os.path.exists('../results'):
 
 os.environ['MAVEN_OPTS'] = '-Xmx100G'
 
-print('window size, CPU time')
-for window_size in range(100, 3700, 100):
-    args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
+print('Window size, CPU time, Peak pool size')
+for window_size in range(args.start, args.stop, args.step):
+    sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
     # print(args)
-    proc = Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    proc.wait()
-    stderr = proc.stderr.read().decode()
-    cpu_time = parse_cputime(stderr)
-    print('{}\t {}'.format(window_size, cpu_time))
+    cpu_time = 0
+    for _ in range(args.re_run):
+        proc = Popen(sub_proc_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc.wait()
+        stderr = proc.stderr.read().decode()
+        stdout = proc.stdout.read().decode()
+
+        cpu_time += parse_cputime(stderr)
+    cpu_time /= args.re_run
+    output = json.loads(stdout)
+    poolsize = output['PeakPoolSize']
+
+    print('{}\t {}\t {}'.format(window_size, cpu_time, poolsize))
