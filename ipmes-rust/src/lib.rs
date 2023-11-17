2d56dc6a4da2af022d5f3ae3e3627ca35e11a17e
Daniel Liu <xyfc128@gmail.com>
2023-11-17 11:08:55 +0800

    remove rust in this branch to avoid confusion

--- ipmes-rust/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod pattern;
-pub mod sub_pattern;
-pub mod input_edge;
-pub mod process_layers;
-pub mod match_edge;
-pub mod sub_pattern_match;
-pub mod pattern_match;
--- ipmes-rust/src/process_layers/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod parse_layer;
-pub mod ord_match_layer;
-pub mod join_layer;
-pub mod naive_join_layer;
-
-pub use parse_layer::ParseLayer;
-pub use ord_match_layer::OrdMatchLayer;
-pub use join_layer::JoinLayer;
