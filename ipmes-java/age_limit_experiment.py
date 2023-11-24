b1f8dd0e2aeb44974a32a6b0ca1bab3e24e911d6
Daniel Liu <xyfc128@gmail.com>
2023-11-24 12:30:53 +0800

    add new experiment options

--- ipmes-java/age_limit_experiment.py
+++ ipmes-java/age_limit_experiment.py
@@ -8,6 +8,10 @@ from results import parse_cputime
 
 parser = parser = argparse.ArgumentParser(
                 description='Age limit experiment')
+parser.add_argument('--darpa',
+                    default=False,
+                    type=bool,
+                    help='run on darpa')
 parser.add_argument('--start',
                     default=100,
                     type=int,
@@ -16,6 +20,10 @@ parser.add_argument('--stop',
                     default=3700,
                     type=int,
                     help='stop of the window size')
+parser.add_argument('--stop-unchanged-poolsize',
+                    default=True,
+                    type=bool,
+                    help='stop when the peak pool size is unchanged')
 parser.add_argument('--step',
                     default=100,
                     type=int,
@@ -39,7 +47,10 @@ if not os.path.exists('../results'):
 os.environ['MAVEN_OPTS'] = '-Xmx100G'
 
 def run(window_size):
-    sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
+    if args.darpa:
+        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="--darpa --regex -w {window_size} ../data/darpa_patterns/TTP1-1_regex ../data/preprocessed/dd1.csv"']
+    else:
+        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
     # print(args)
     cpu_time = 0
     for _ in range(args.re_run):
@@ -55,6 +66,8 @@ def run(window_size):
 
     print('{}\t {}\t {}'.format(window_size, cpu_time, poolsize))
 
+    return cpu_time, poolsize
+
 print('Window size, CPU time, Peak pool size')
 if args.multiplier == 1:
     for window_size in range(args.start, args.stop, args.step):
@@ -62,6 +75,11 @@ if args.multiplier == 1:
 else:
     window_size = args.start
     mul = args.multiplier
-    while window_size < args.stop:
-        run(window_size)
+    stop = args.stop if args.stop > args.start else float('inf')
+    last_pool_size = -1
+    while window_size < stop:
+        cpu_time, pool_size = run(window_size)
+        if pool_size == last_pool_size and args.stop_unchanged_poolsize:
+            break
+        last_pool_size = pool_size
         window_size *= mul
