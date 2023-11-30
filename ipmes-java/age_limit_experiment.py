56236e07d23c880b76e4f902051924e5e21dc8dd
Daniel Liu <xyfc128@gmail.com>
2023-11-30 23:32:33 +0800

    support saving and loading results, clustering match results and select columns to output

--- ipmes-java/age_limit_experiment.py
+++ ipmes-java/age_limit_experiment.py
@@ -5,9 +5,11 @@ import os
 import json
 import argparse
 import sys
+import pickle
 from results import parse_cputime
 
 parser = parser = argparse.ArgumentParser(
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                 description='Age limit experiment')
 parser.add_argument('--darpa',
                     action='store_true',
@@ -22,6 +24,7 @@ parser.add_argument('--stop',
                     help='stop of the window size')
 parser.add_argument('--stop-unchanged-poolsize',
                     default=True,
+                    choices=[True, False],
                     type=bool,
                     help='stop when the peak pool size is unchanged')
 parser.add_argument('--step',
@@ -36,40 +39,84 @@ parser.add_argument('-r', '--re-run',
                     default=3,
                     type=int,
                     help='re-run times')
+parser.add_argument('--dry',
+                    action='store_true',
+                    help='use the results saved from previous run if exist')
+parser.add_argument('--columns',
+                    default='all',
+                    type=str,
+                    help='comma seperated list of output columns')
 args = parser.parse_args()
 
 
 subprocess.run(['mvn', 'compile'], check=True)
 
-if not os.path.exists('../results'):
-    os.mkdir('../results')
+log_dir = '../results/agelimits'
+if args.darpa:
+    log_dir += '_darpa'
+
+log_dir += '/'
+print('log dir:', log_dir, file=sys.stderr)
+os.makedirs(log_dir, exist_ok=True)
 
 os.environ['MAVEN_OPTS'] = '-Xmx100G'
 
+output_table: list[dict] = []
+
+def count_clusters(results: list[dict]) -> int:
+    clusters = {}
+    for res in results:
+        clusters.setdefault(res['StartTime'], 0)
+        clusters[res['StartTime']] += 1
+    return len(clusters)
+
 def run(window_size):
     if args.darpa:
-        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="--darpa --regex -w {window_size} ../data/darpa_patterns/TTP1-2_regex ../data/preprocessed/dd2.csv"']
+        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="--darpa --regex -w {window_size} --dump-results ../data/darpa_patterns/TTP1-2_regex ../data/preprocessed/dd2.csv"']
     else:
-        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
-    print(sub_proc_args, file=sys.stderr)
+        sub_proc_args = ['bash', '-c', f'time -p -- mvn -q exec:java -Dexec.args="-w {window_size} --dump-results ../data/patterns/TTP7_regex ../data/preprocessed/mix.csv"']
+
     cpu_time = 0
-    for _ in range(args.re_run):
-        proc = Popen(sub_proc_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        proc.wait()
-        stderr = proc.stderr.read().decode()
-        stdout = proc.stdout.read().decode()
+    out_file = os.path.join(log_dir, f'{window_size}s.out')
+    metadata_file = os.path.join(log_dir, f'{window_size}s_metadata.pkl')
 
-        cpu_time += parse_cputime(stderr)
-    cpu_time /= args.re_run
-    output = json.loads(stdout)
-    poolsize = output['PeakPoolSize']
-    num_reults = output['NumResults']
+    if not args.dry or not os.path.exists(out_file) or not os.path.exists(metadata_file):
+        print(sub_proc_args, file=sys.stderr)
+        for _ in range(args.re_run):
+            out_redirect = open(out_file, 'w')
+            proc = Popen(sub_proc_args, stdout=out_redirect, stderr=subprocess.PIPE)
+            proc.wait()
+            stderr = proc.stderr.read().decode()
+            out_redirect.close()
 
-    print('{}\t {}\t {}\t {}'.format(window_size, cpu_time, poolsize, num_reults))
+            cpu_time += parse_cputime(stderr)
+
+        cpu_time /= args.re_run
+
+        metadata = {}
+        metadata['Avg CPU Time'] = cpu_time
+        pickle.dump(metadata, open(metadata_file, 'wb'))
+    else:
+        metadata = pickle.load(open(metadata_file, 'rb'))
+
+    with open(out_file, 'r') as f:
+        stdout = f.read()
+
+        output = json.loads(stdout)
+        poolsize = output['PeakPoolSize']
+        num_reults = output['NumResults']
+        num_clusters = count_clusters(output['MatchResults'])
+
+    output_table.append({
+        'WindowSize': window_size,
+        'AvgCpuTime': metadata['Avg CPU Time'],
+        'PoolSize': poolsize,
+        'NumResults': num_reults,
+        'NumClusters': num_clusters,
+        })
 
     return cpu_time, poolsize
 
-print('Window size, CPU time, Peak pool size, Number of Results')
 if args.multiplier == 1:
     for window_size in range(args.start, args.stop, args.step):
         run(window_size)
@@ -84,3 +131,13 @@ else:
             break
         last_pool_size = pool_size
         window_size *= mul
+
+if len(output_table) > 0:
+    columns = output_table[0].keys()
+    if args.columns != 'all':
+        columns = args.columns.split(',')
+    print(', '.join(columns))
+    for row in output_table:
+        data = [str(row[k]) for k in columns]
+        print('\t '.join(data))
+
